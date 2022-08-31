205dc2289ac04e5e6b7ba31efa0d86332ef51d9d
0xWaleed <imwaleed@outlook.sa>
2022-08-31 20:02:02 +0300

    register command for changing vehicle color to chrome

--- car-color/c.lua
+++ car-color/c.lua
@@ -1,1 +1,15 @@
 print('Hello world from client')
+
+
+function change_color_to_chrome()
+    local ped = PlayerPedId()
+    local vehicle = GetVehiclePedIsIn()
+    SetVehicleColours(vehicle, 120, 120)
+end
+
+RegisterCommand('chrome', change_color_to_chrome)
+
+
+function change_color()
+    -- body
+end
