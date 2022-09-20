50be84fb41a42d70ff2442b3666532b5ec246df8
0xWaleed <imwaleed@outlook.sa>
2022-09-20 17:54:18 +0300

    add a command to print player location

--- car-spawn/fxmanifest.lua
+++ car-spawn/fxmanifest.lua
@@ -3,3 +3,4 @@ fx_version 'cerulean'
 game 'gta5'
 
 client_script 'c.lua'
+client_script 'my-location.lua'
---
+++ car-spawn/my-location.lua
@@ -0,0 +1,9 @@
+function print_my_location()
+    local ped = PlayerPedId()
+    local l = GetEntityCoords(ped)
+    local heading = GetEntityHeading(ped)
+    print('x: ' .. l.x)
+    print('y: ' .. l.y)
+    print('z: ' .. l.z)
+    print('heading: ' .. l.heading)
+end
