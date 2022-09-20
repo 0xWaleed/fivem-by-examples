f35a54fbb8518020e6d3305efa6101f7325b0add
0xWaleed <imwaleed@outlook.sa>
2022-09-20 17:29:18 +0300

    delete vehicle before spawning

--- car-spawn/c.lua
+++ car-spawn/c.lua
@@ -1,21 +1,24 @@
+local currentVehicle = 0
+
 function get_my_location()
     local ped = PlayerPedId()
     local location = GetEntityCoords(ped)
     return location
 end
 
-function spawn_vehicle(modelName, x, y, z)
+function spawn_vehicle(modelName, x, y, z, heading)
     local hash = GetHashKey(modelName)
     RequestModel(hash)
     repeat Wait(10) until HasModelLoaded(hash)
-    local vehicle = CreateVehicle(hash, x, y + 5, z, 0, true, true)
+    local vehicle = CreateVehicle(hash, x, y + 5, z, heading or 0, true, true)
     return vehicle
 end
 
 function spawn_vehicle_command_wrapper(modelName)
     return function()
+        DeleteVehicle(currentVehicle)
         local loc = get_my_location()
-        spawn_vehicle(modelName, loc.x, loc.y, loc.z)
+        currentVehicle = spawn_vehicle(modelName, loc.x, loc.y, loc.z)
     end
 end
 
